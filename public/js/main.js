90fa0ac47a79974593203d708939a8d79ff57355
jennvance <vancejenniferm@gmail.com>
2016-09-25 16:45:45 -0600

    accounted for all possible search outcomes

--- public/js/main.js
+++ public/js/main.js
@@ -5,8 +5,10 @@ angular.module('app')
 		var s = $scope;
 		s.test = "Hi Jenn";
 		//not necessary, but semantically useful
-		s.user = {};
+		s.submittedUser = {};
+		s.currentUser = {};
 		s.users=[];
+		s.justentered = true;
 
 		s.User = function(user){
 			this.name = user.name;
@@ -19,12 +21,14 @@ angular.module('app')
 		
 
 		s.submitLogin = function(){
+
 			//if users is empty, add new user
 			if(s.users==false){
 				console.log("users is empty; creating first item")
-				s.user = new s.User(s.user);
-				s.users.push(s.user)
+				s.currentUser = new s.User(s.user);
+				s.users.push(s.currentUser)
 				s.user = {};
+				s.justentered = false;
 			} else {
 				//search users for name that matches
 				for(var i=0;i<s.users.length; i++){
@@ -33,14 +37,21 @@ angular.module('app')
 						console.log("user exists, displaying data")
 						//display data and return out of the for loop
 						console.log(i + " name: " + s.user.name)
+						s.currentUser = s.user;
+						s.user = {};
+						console.log("currentUser: " + s.currentUser)
+						s.justentered = false;
 						return false;
 					}
 				}
 				//if get to end of loop and no match, add new user
 				console.log("user doesn't exist, adding user to populated array")
-				s.user = new s.User(s.user);
-				s.users.push(s.user)
+				s.currentUser = new s.User(s.user);
+				console.log(s.currentUser.name)
+				s.users.push(s.currentUser)
+				
 				s.user = {};
+				s.justentered = false;
 			}
 
 
